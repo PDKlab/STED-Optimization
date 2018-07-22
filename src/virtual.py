d02ba171f65257dee7d1bababab1410da72c7c2b
Audrey Durand <audrey.durand@novasyst.com>
2018-07-22 16:52:24 -0400

    Update interface for quality rating neural network

--- src/virtual.py
+++ src/virtual.py
@@ -21,49 +21,23 @@ class QualityNet:
         self.port = port
         self.url = 'http://{}:{}'.format(self.address, self.port)
 
-    def predict(self, sted_confocal):
+    def predict(self, sted):
         """Predict the quality score of an image using a remote neural network.
 
-        :param sted_confocal (str/tuple): Path of saved numpy arrays or tuple of numpy
-                                          arrays for sted and confocal
+        :param sted (str/tuple): Path of saved numpy array or numpy array for sted
 
         .. note::
 
-            The data type of image and mask contained (or path given) in `sted_confocal`
+            The data type of image and mask contained (or path given) in `sted`
             data must be [0, 1] floats.
 
         :returns: Predicted quality score (float) in [0, 1].
         """
-        if isinstance(sted_confocal, str):
-            npz = np.load(sted_confocal)
-            # img, mask = npz['arr_0'], npz['arr_1']
-            sted, confocal = npz['arr_0'], npz['arr_1']
-        elif isinstance(sted_confocal, tuple) or isinstance(sted_confocal, list) :
-            # img, mask = img_mask
-            sted, confocal = sted_confocal
-        else:
-            print(' [!] img must be a str or a numpy array')
-
-        width, height = sted.shape
-        mask_width = int(np.ceil(width / 16))
-        mask_height = int(np.ceil(height / 16))
+        if isinstance(sted, str):
+            sted = np.load(sted)
         
-        # foreground on confocal image
-        fg_c = utils.get_foreground(confocal)
-        # foreground on sted image
-        fg_s = utils.get_foreground(sted)
-        # remove STED foreground points not in confocal foreground, if any
-        fg = fg_s * fg_c
-        
-        # compute binary background mask
-        mask = resize(fg, (mask_width, mask_height))
-        mask[mask > 0] = 1.0
-        mask = (mask / np.sum(mask)) if np.sum(mask) > 0 else mask
-        
-        tosend = json.dumps({'img':sted.tolist(),
-                             'img-type':'{}'.format(sted.dtype),
-                             'mask': mask.tolist(),
-                             'mask-type':'{}'.format(mask.dtype)})
+        tosend = json.dumps({'img': sted,
+                             'img-type': '{}'.format(sted.dtype)})
         print("Asking QualityNet...")
         r = requests.post(self.url, data=tosend)
         print("QualityNet call result", r.text)
